b6d37e81dde5b02887984b754a987914e128b734
liangyutao <liangyutao@yiche.com>
2021-01-19 19:46:08 +0800

    滑动窗口

---
+++ src/com/atyiche/algorithm/sliding_window/SlidingWindowMaximum.java
@@ -0,0 +1,97 @@
+package com.atyiche.algorithm.sliding_window;
+
+import java.util.ArrayDeque;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * @author liangyt
+ * @create 2021-01-19 15:42
+ */
+//滑动窗口最大值
+public class SlidingWindowMaximum {
+    //方法一：暴力法，遍历每一个窗口，对每个窗口遍历每个元素求最大值
+    public int[] maxSlidingWindow1(int[] nums, int k) {
+        //定义一个结果数组，总共有n-k+1个窗口
+        int[] result = new int[nums.length - k + 1];
+        //遍历数组，从0到n-k，作为滑动窗口的起始位置
+        for (int i = 0; i <= nums.length - k; i++) {
+            //找窗口内的最大值，定义一个变量来保存
+            int max = nums[i];
+            //遍历窗口中的每一个元素比较大小
+            for (int j = i + 1; j < i + k; j++) {
+                if (nums[j] > max){
+                    max = nums[j];
+                }
+            }
+            result[i] = max;
+        }
+        return result;
+    }
+
+    //方法二：使用大顶堆
+    public int[] maxSlidingWindow2(int[] nums, int k) {
+        //定义一个结果数组
+        int[] result = new int[nums.length - k + 1];
+        //用优先队列实现一个大顶堆
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(k, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return  o2 - o1;
+            }
+        });
+        //准备工作：构建大顶堆，将第一个窗口元素（k个）放入堆中
+        for (int i = 0; i < k; i++) {
+            maxHeap.add(nums[i]);
+        }
+        //当前大顶堆的顶堆元素就是第一个窗口的最大值
+        result[0] = maxHeap.peek();
+        //遍历所有窗口
+        for (int i = 1; i <= nums.length - k; i++) {
+            //删除上一个窗口中的元素
+            maxHeap.remove(nums[i -1]);
+            //添加当前的最后一个元素进堆
+            maxHeap.add(nums[i + k -1]);
+            result[i] = maxHeap.peek();
+        }
+        return result;
+    }
+
+    //方法三：使用双向队列
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        //定义一个结果数组
+        int[] result = new int[nums.length - k + 1];
+        //定义双向队列,保存元素的索引
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+        //初始化双向队列，处理第一个窗口数据
+        for (int i = 0; i < k; i++) {
+            while (!deque.isEmpty() && nums[i] > nums[deque.getLast()])
+                deque.removeLast();
+            deque.addLast(i);
+        }
+        result[0] = nums[deque.getFirst()];//第一个窗口的最大值
+        //遍历数组中的所有元素,作为窗口的起始位置
+        for (int i = k; i < nums.length; i++) {
+           //如果上一个窗口删掉的就是窗口最大值，那么需要将队列中的最大值删掉
+            if (!deque.isEmpty() && deque.getFirst() == i - k)
+                deque.removeFirst();
+            //判断新增元素是否可以删除队尾元素
+            //如果队尾元素小于当前元素，直接删除
+            while (!deque.isEmpty() && nums[i] > nums[deque.getLast()])
+                deque.removeLast();
+            deque.addLast(i);
+            //保存结果
+            result[i - k + 1] = nums[deque.getFirst()];
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        int[] input = {1,3,-1,-3,5,3,6,7};
+        int k = 3;
+        SlidingWindowMaximum slidingWindowMaximum = new SlidingWindowMaximum();
+        int[] output = slidingWindowMaximum.maxSlidingWindow(input,k);
+        for (int max : output) {
+            System.out.print(max + "\t");
+        }
+    }
+}
--- src/com/atyiche/algorithm/strings/RemoveDuplicateLetters.java
+++ src/com/atyiche/algorithm/strings/RemoveDuplicateLetters.java
@@ -1,12 +1,16 @@
 package com.atyiche.algorithm.strings;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Stack;
+
 /**
  * @author liangyt
  * @create 2021-01-18 19:53
  */
 public class RemoveDuplicateLetters {
     //方法一：暴力法，贪心策略递归
-    public String removeDuplicateLetters(String s){
+    public String removeDuplicateLetters1(String s){
         //递归的基本情形
         if (s.length()==0)
             return "";
@@ -40,9 +44,70 @@ public class RemoveDuplicateLetters {
             }
         }
         //遍历结束，position位置的字母就是结果中最左侧的元素
-        return s.charAt(position) + removeDuplicateLetters(s.substring(position+1).replaceAll(s.charAt(position) + "",""));
+        return s.charAt(position) + removeDuplicateLetters1(s.substring(position+1).replaceAll(s.charAt(position) + "",""));
     }
 
+    //方法二：贪心策略改进
+    public String removeDuplicateLetters2(String s){
+        //递归的基本情形
+        if (s.length()==0)
+            return "";
+        //希望找到当前最左侧的字母，位置记为position
+        int position = 0;
+        //定义一个count数组，保存所有26个字母在字符串中出现的频次
+        int count[] = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a'] ++;//count[0]保存a的个数，count[1]保存b的个数
+        }
+        //遍历字符串，找到当前最左端字母
+        for (int i = 0; i < s.length(); i++) {
+            //把当前字符和position位置比较，如果更小就替换
+            if (s.charAt(i) < s.charAt(position))
+                position = i;
+            //每遇到一个字符，count值就减1
+            //如果遇到count减为0，就直接退出，以当前最小的字母作为最左端字符
+            if (--count[s.charAt(i) - 'a'] == 0)
+                break;
+        }
+        //遍历结束，position位置的字母就是结果中最左侧的元素
+        return s.charAt(position) + removeDuplicateLetters2(s.substring(position + 1).replaceAll(s.charAt(position) + "",""));
+    }
+
+    //方法三：使用栈进行优化
+    public String removeDuplicateLetters(String s){
+        //定义一个字符栈，保存去重后的结果
+        Stack<Character> stack = new Stack<>();
+
+        //为了快速判断一个字符是否在栈中出现过，用一个set来保存元素是否出现过
+        HashSet<Character> seenSet = new HashSet<>();
+
+        //为了快速判断一个字符是否在某个位置重复出现，用hashmap来保存字母出现在字符串的最后位置
+        HashMap<Character, Integer> lastrOccur = new HashMap<>();
+
+        //遍历字符串，将最后一次出现的位置保存进map
+        for (int i = 0; i < s.length(); i++) {
+            lastrOccur.put(s.charAt(i),i);
+        }
+        //遍历字符串，判断每个字符是否需要入栈
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            //在c不存在的情况下考虑入栈
+            if (!lastrOccur.containsKey(c)){
+                //c入栈之前，要判断之前的栈顶元素，是否会在后面重复出现，如果重复出现即可删除
+                while (!stack.isEmpty() && c < stack.peek() && lastrOccur.get(stack.peek()) > i){
+                    seenSet.remove(stack.pop());
+                }
+                stack.push(c);
+                seenSet.add(c);
+            }
+        }
+        //将栈中的元素保存为字符串输出
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Character c : stack){
+            stringBuilder.append(c.charValue());
+        }
+        return stringBuilder.toString();
+    }
     public static void main(String[] args) {
         String str1 = "bcabc";
         String str2 = "cbacdcbc";
